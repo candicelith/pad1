bd825ce734ff48991edadbba044cc8c31e5a498b
candicelith <irenetalithatyasraharjo@mail.ugm.ac.id>
2024-10-17 17:57:34 +0700

    Refactor header and footer links to use route names

--- resources/views/layout/headerFooter.blade.php
+++ resources/views/layout/headerFooter.blade.php
@@ -19,11 +19,11 @@
     <header id="navbar" class="bg-white border-gray-200 sticky top-0 z-50 transition-shadow duration-300">
         <nav>
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="{{ route('home') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="{{ asset('assets/brand-blue 1.svg') }}" class="w-[121px] h-[58px]" alt="Pokari Logo" />
                 </a>
                 <button data-collapse-toggle="navbar-default" type="button"
-                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-400 rounded-lg md:hidden hover:bg-cyan hover:text-white focus:outline-none focus:ring-2 focus:ring-cyan"
                     aria-controls="navbar-default" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -34,7 +34,7 @@
                 </button>
                 <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul
-                        class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        class="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-white md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
                         <li>
                             <a href="{{ route('home') }}"
                                 class="block py-2 px-3 {{ request()->routeIs('home') ? 'text-cyan' : 'text-gray-400' }} md:hover:text-cyan text-xl rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0"
@@ -80,7 +80,7 @@
                 <a href="#" class="flex justify-center items-center">
                     <img src="{{ asset('assets/logo-TRPL 1.svg') }}" class="w-[178px] h-[80px]" alt="Pokari Logo" />
                 </a>
-                <a href="#" class="flex justify-center items-center">
+                <a href="{{ route('home') }}" class="flex justify-center items-center">
                     <img src="{{ asset('assets/brand-white 2.svg') }}" class="w-[178px] h-[80px]" alt="Pokari Logo" />
                 </a>
             </div>
