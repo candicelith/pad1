e5c3e2356b9b10d2002d30e4f804077fc216ca7c
candicelith <irenetalithatyasraharjo@mail.ugm.ac.id>
2025-03-07 11:27:31 +0700

    Refactor login page layout and add Google sign-in option

--- resources/views/content/login.blade.php
+++ resources/views/content/login.blade.php
@@ -4,79 +4,31 @@
     <section class="mt-28 bg-white sm:mt-0">
         <div class="mx-auto my-16 flex max-w-screen-xl items-center justify-center md:h-screen lg:py-16">
             <div
-                class="mx-10 w-full rounded-lg bg-cyan-100 px-8 py-10 shadow-lg sm:max-w-xl sm:px-14 sm:py-16 lg:max-w-5xl lg:px-20 xl:max-w-6xl">
-                <h1 class="text-center text-2xl leading-tight tracking-tight text-white sm:text-3xl md:text-4xl">
-                    Login
-                </h1>
-                <p class="my-4 text-center text-sm text-white sm:text-base">Please log in if you are part of UGM-Software
-                    Engineering
-                    students
-                </p>
-                <form class="mx-auto max-w-4xl space-y-6" action="{{ route('authenticate') }}" method="POST">
-                    @csrf
-                    {{-- Email Input --}}
-                    <div class="flex flex-col lg:flex-row lg:items-center lg:space-x-6">
-                        <label for="email" class="mb-2 shrink-0 text-base text-white sm:text-lg lg:mb-0 lg:w-40">
-                            E-mail
-                        </label>
-                        <input type="email" name="email" id="email" {{-- Nama : email --}}
-                            class="focus:ring-primary-600 focus:border-primary-600 w-full rounded-full border border-gray-300 bg-gray-50 text-sm text-gray-900 placeholder:text-sm sm:p-4 sm:text-base"
-                            placeholder="Enter your UGM E-mail" required />
-                    </div>
-
-                    {{-- Password Input --}}
-                    <div class="flex flex-col lg:flex-row lg:items-center lg:space-x-6">
-                        <label for="password" class="mb-2 shrink-0 text-base text-white sm:text-lg lg:mb-0 lg:w-40">
-                            Password
-                        </label>
-                        <input type="password" name="password" id="password" {{-- Nama : password --}}
-                            class="focus:ring-primary-600 focus:border-primary-600 w-full rounded-full border border-gray-300 bg-gray-50 text-sm text-gray-900 placeholder:text-sm sm:p-4 sm:text-base"
-                            placeholder="Enter your NIU" required />
-                    </div>
-
-                    {{-- Log In Button --}}
-                    <div class="flex justify-end">
-                        <button type="submit"
-                            class="w-full rounded-full border border-gray-900 bg-white px-5 py-1 text-base text-cyan hover:bg-cyan hover:text-white focus:ring-4 focus:ring-cyan sm:py-3 sm:text-lg lg:w-48">
-                            Log In
-                        </button>
-
-                        {{-- Popup Modal Trigger --}}
-                        @if ($errors->has('email') || $errors->has('password'))
-                            {{-- Error Listener --}}
-                            <script>
-                                document.addEventListener("DOMContentLoaded", function() {
-                                    document.getElementById("popup-modal").classList.remove("hidden");
-                                    document.getElementById("modal-backdrop").classList.remove("hidden");
-                                });
-                            </script>
-                        @endif
-                    </div>
-                    {{-- <button type="button" class="flex flex-row justify-center gap-3 w-full py-2 border border-black text-black font-bold rounded-md hover:bg-gray-50" onclick="window.location.href='/auth/google/redirect'">
-                        <p>Sign In with</p>
-                        <img src="assets/google.png" alt="Google" width="25">
-                    </button> --}}
-
-                    {{-- Backdrop for Modal --}}
-                    <div id="modal-backdrop" class="fixed inset-0 z-40 hidden bg-black bg-opacity-50 backdrop-blur-sm">
-                    </div>
-                    {{-- popup modal --}}
-                    <div id="popup-modal" tabindex="-1"
-                        class="fixed left-1/2 top-1/2 z-50 hidden w-full max-w-md -translate-x-1/2 -translate-y-1/2 p-4">
-                        <div class="relative rounded-lg bg-red-300 shadow">
-                            <div class="p-4 text-center md:p-5">
-                                <h3 class="mb-5 text-lg font-normal text-red-950">
-                                    Login failed. Please check your email or password and try again.
-                                </h3>
-                                <button data-modal-hide="popup-modal" type="button" onclick="closeModal()"
-                                    class="ms-3 rounded-full border border-gray-900 bg-white px-5 py-2.5 text-sm font-medium text-red-950 hover:bg-red-950 hover:text-white focus:z-10 focus:outline-none focus:ring-4 focus:ring-red-950">
-                                    Close
-                                </button>
-                            </div>
+                class="mx-10 w-full rounded-3xl bg-cyan-100 px-24 py-10 shadow-lg sm:max-w-xl md:px-24 md:py-11 lg:max-w-5xl xl:max-w-6xl">
+                <div class="flex items-center justify-between">
+                    <div class="flex w-80 flex-col space-y-20">
+                        <div class="space-y-3 text-white">
+                            <p class="text-start text-base sm:text-xl">Welcome!
+                            </p>
+                            <h1 class="text-start text-2xl leading-tight tracking-tight md:text-5xl">
+                                Sign In
+                            </h1>
+                        </div>
+                        <div class="space-y-3 text-white">
+                            <h2 class="text-2xl">Your Future Starts Here!</h2>
+                            <p class="text-wrap text-base">Sign in to access alumni career paths,
+                                job postings, and company insights.</p>
                         </div>
                     </div>
+                    <div class="flex flex-col items-center space-y-3">
+                        <div class="flex items-center justify-center rounded-xl bg-white pe-8">
+                            <img src="{{ asset('assets/Google Icon.svg') }}" alt="" class="p-4">
+                            <button class="px-4 py-5 text-center text-lg">Sign In With Google</button>
+                        </div>
+                        <p class="text-sm text-white">Sign in with your UGM account</p>
+                    </div>
+                </div>
 
-                </form>
             </div>
         </div>
 
