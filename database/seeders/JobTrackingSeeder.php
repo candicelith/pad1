89fe3d50d4ffdcba881d548003100a74627ba896
Fagan <mustafafagan@gmail.com>
2025-06-18 22:51:50 +0700

    feat : deployment setup and finalization

--- database/seeders/JobTrackingSeeder.php
+++ database/seeders/JobTrackingSeeder.php
@@ -5,24 +5,28 @@ namespace Database\Seeders;
 use App\Models\JobTracking;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class JobTrackingSeeder extends Seeder
 {
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
+        $faker = Faker::create();
+
         for ($i = 0; $i < 100; $i++) {
             JobTracking::create([
-                'id_userDetails' => fake()->numberBetween(1, 38),
-                'id_jobs' => fake()->numberBetween(1, 25),
-                'date_start' => fake()->date(),
-                'date_end' => fake()->optional()->date(),
+                'id_userDetails' => $faker->numberBetween(1, 38),
+                'id_jobs' =>$faker->numberBetween(1, 25),
+                'date_start' =>$faker->date(),
+                'date_end' =>$faker->optional()->date(),
                 'job_description' => [
-                    fake()->sentence(),
-                    fake()->sentence(),
-                    fake()->sentence(),
+                   $faker->sentence(),
+                   $faker->sentence(),
+                   $faker->sentence(),
                 ],
                 'created_at' => now(),
                 'updated_at' => now()
